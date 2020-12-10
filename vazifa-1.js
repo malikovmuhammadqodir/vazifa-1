61457fbc1fb788a9bed7588cd59b0a5293faa1ca
malikovmuhammadqodir <75750838+malikovmuhammadqodir@users.noreply.github.com>
2020-12-10 19:23:47 +0500

    homework

---
+++ vazifa-1.js
@@ -0,0 +1,332 @@
+// #1
+// son = 6;
+// const p = 4 * son;
+// console.log(p)
+
+// #2
+//  const son = 4;
+//  let S = (Math.pow(8,2));
+//  console.log(S)
+
+// #3
+//  let a = 10;
+//  let b = 6;
+//  let S = a * b;
+//  console.log(S)
+//  let P = 2 * (a + b);
+//  console.log(P)
+
+// #4
+// let d = 12;
+// const L = (Math.PI * d);
+// console.log(L)
+
+// #5
+// const a = 16;
+// let V = (Math.pow(a,3));
+// console.log(V)
+// let S = (Math.pow(a,2) * 6);
+// console.log(S)
+
+// #6
+// const a = 4;
+// const b = 6;
+// const c = 8;
+// let V = a * b * c;
+// console.log(V)
+// let S = 2 * (a * b + b * c + a * c);
+// console.log(S)
+
+// #7
+// const R = 8;
+// let L = (2 * Math.PI * R);
+// console.log(L)
+// let S = (Math.PI * Math.pow(R,2));
+// console.log(S)
+
+// # 8
+// const a = 20;
+// const b = 28;
+// console.log((a + b)/2)
+
+// #9
+// const a = 16;
+// const b = 12;
+// console.log(Math.sqrt(a * b))
+
+// #10
+// const son1 = 22;
+// const son2 = 28;
+// console.log(son1 + son2)
+// console.log(son1 * son2)
+// console.log(Math.pow(son1,2))
+// console.log(Math.pow(son2,2))
+
+//#11
+// const son1 = 23;
+// const son2 = 16;
+// console.log(son1 + son2)
+// console.log(son1 * son2)
+// console.log(son1 % 10)
+// console.log(son2 % 10)
+
+//#12
+
+// const a = 9;
+// const b = 14;
+// let c = (Math.sqrt(Math.pow(a,2)+(Math.pow(b,2))));
+// console.log(c)
+// let P = a + b + c;
+// console.log(P)
+
+// #13
+// const R1 = 32;
+// const R2 = 28;
+// let S1 = (Math.PI * R1);
+// let S2 = (Math.PI * R2);
+// let S3 = (Math.PI *(R1 - R2));
+// console.log('S1=' + S1)
+// console.log('S2=' + S2)
+// console.log('S3=' + S3)
+
+// # 14
+
+// const l = 29;
+// let R = 2 * Math.PI * l;
+// console.log(R)
+// let S = Math.PI * (R * R);
+// console.log(S)
+
+// #15
+
+// const R = 15;
+// let L = 2 * Math.PI * R;
+// console.log(L)
+// let S = Math.PI * (R * R);
+// console.log(S)
+
+// #17
+
+// const a = -13;
+// const b = 8;
+// const c = 4;
+// let a_b = a + b;
+// console.log(a_b)
+// let b_c = b + c;
+// console.log(b_c)
+// let res = a_b + b_c;
+// console.log(res)
+
+// #18
+
+// const a = 6;
+// const b = 12;
+// const c = -4;
+// let a_b = a + b;
+// console.log(a_b)
+// let b_c = b + c;
+// console.log(b_c)
+// let res = a_b * b_c;
+// console.log(res)
+
+// # 19
+
+// const a = 20;
+// const b = 18;
+// let p = 4 *(a + b);
+// console.log(p)
+// let S = a * b;
+// console.log(S)
+
+// # 20
+// const x1 = 8;
+// const x2 = 16;
+// const y1 = 12;
+// const y2 = 4;
+// let res = (Math.sqrt(Math.pow(x2 - x1)+(Math.pow(y1 - y2))));
+// console.log(res)
+
+// #22
+
+// let a = 10;
+// let b = 40;
+// console.log('a='+b)
+// console.log('b='+a)
+
+// #21
+
+// const a = 20;
+// const b = 18;
+// const c = 30;
+// let p = (a + b + c) / 2;
+// console.log(p)
+// let S = p / (p - a)*(p - b)*(p - c);
+// console.log(Math.sqrt(S))
+
+// # 23
+//  let a = 50;
+//  let b = 20;
+//  let c = 10;
+//  console.log('a='+b)
+//  console.log('b='+a)
+//  console.log('c='+ b)
+
+// # 25
+
+// const x = 6;
+// let y = 3 * (Math.pow(x,6)) - 6 * (Math.pow(6,6)) - 7;
+// console.log(y);
+
+//#26
+//  const x = 8;
+//  let y = 4 * (x - Math.pow(3,6)) - 7 * (x - Math.pow(3,3)) + 2;
+//  console.log(y)
+
+//27
+// const A = 9
+// console.log(Math.pow(A,2))
+// console.log(Math.pow(A,4))
+// console.log(Math.pow(A,8))
+
+//28
+// const A = 7
+// console.log(Math.pow(A,2),Math.pow(A,3),Math.pow(A,3),Math.pow(A,5),Math.pow(A,10),Math.pow(A,15))
+
+//29
+
+
+// Butun songa oid masalalar!!
+
+// 1
+
+// let l = 500;
+// console.log(l / 100)
+
+// 2
+
+// let m = 4302;
+// console.log(m / 1000)
+
+// 3
+
+// let bayt = 4809;
+// console.log(bayt / 1024 % 100)
+
+// 4
+
+// let a = 99;
+// let b = 33;
+// console.log(a / b + 'marta mumkin')
+
+// 5
+// let a = 43;
+// let b = 28;
+// console.log(a / b % 10);
+
+//6
+
+// let num = 98;
+// console.log(num % 100)
+// console.log(num % 10)
+
+//7
+
+// let num = 67;
+// let num1 = num % 10;
+// let num2 = num / 10;
+// console.log(num2 + num1)
+
+//9
+
+// let num = 459;
+// console.log(num / 100)
+
+//10
+
+// let num = 873;
+// console.log(num % 10 / 1)
+// console.log(num % 100)
+
+//11
+
+// let num = 376;
+// let birlik = num % 10 / 1;
+// let onlik = num / 10 % 10;
+// let yuzlik = num / 100;
+// console.log(birlik + onlik + yuzlik)
+
+//13
+
+// let num = 654;
+// let birlik = num % 10 / 1;
+// let onlik = num / 10 % 10;
+// let yuzlik = num / 100;
+// console.log(birlik,onlik,yuzlik)
+
+//14
+
+// let num = 324;
+// let birlik = num % 10 / 1;
+// console.log(birlik);
+
+//15
+
+// const num = 123;
+// let birlik = num % 10;
+// let onlik = num / 10 % 10;
+// let yuzlik = num / 100;
+// console.log(onlik,yuzlik,birlik);
+
+//16
+
+// const num = 123;
+// let birlik = num % 10;
+// let onlik = num / 10 % 10;
+// let yuzlik = num / 100;
+// console.log(onlik,birlik,yuzlik)
+
+//17
+
+// const num = 1750;
+// let qoldiq = num / 2 % 100;
+// console.log(qoldiq % 100);
+
+// 18
+// const num = 1220;
+// let butun = num / 2;
+// let qoldiq = num % 1000;
+// let minglik = num / 1000;
+// console.log('minglik=',minglik, 'butun=',butun, 'qoldiq=', qoldiq);
+
+// 19
+
+// const sekund = 3200;
+// let daqiqa = sekund / 60;
+// console.log(daqiqa);
+
+//20
+
+// const sekund = 4500;
+// let soat = sekund / 3600;
+// console.log(soat);
+
+//21
+
+// const sekund = 5430;
+// let daqiqa = sekund / 60;
+// let soat = sekund / 3600;
+// console.log('daqiqa=',daqiqa, 'soat=',soat);
+
+// 22
+// const sekund = 3430;
+// let daqiqa = sekund / 60;
+// let soat = sekund / 3600;
+// console.log('soat=',soat, 'daqiqa=',daqiqa,);
+
+//23
+
+const sek = 4000;
+let sekund = sek * 1;
+let daqiqa = sek / 60
+let soat = sek / 3600;
+console.log('sekund=',sekund,'daqiqa=',daqiqa ,'soat=', soat);
