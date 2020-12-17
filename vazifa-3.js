8e32433b3c24cf67de8e28b386cb60de13abbd5a
malikovmuhammadqodir <75750838+malikovmuhammadqodir@users.noreply.github.com>
2020-12-17 19:33:56 +0500

    vazifa-3.js

---
+++ vazifa-3.js
@@ -0,0 +1,187 @@
+    
+// for skill operatoriga oid masalalar
+
+//1
+// let k = 8;
+// let n = 10;
+// for (let k=1;k<=n;k++) {
+//     console.log(k)
+// }
+
+//2
+let a = 9;
+let b = 7;
+let sum = a
+for (let i=1;i<=sum;i++) {
+    sum = a+b;
+    console.log(sum);
+}
+
+//3 ????
+
+//4
+// let narx = 8000;
+// for(let i=1;i<=10;i++) {
+//     console.log(i,'kg =',i * narx,'narx');
+// }
+
+//5
+// narx = 6000;
+// for (let i=0.1;i<=10;i++) {
+//     console.log(i,'kg',i*narx,'narx')
+// }
+
+//6
+// narx = 5000;
+// for (let i=1.2;i<=2;i++) {
+//     console.log(i,'kg',i*narx,'narx')
+// }
+
+//7
+// let a = 2;
+// let b = 20;
+// for (let i=1;i<=b;i++) {
+//     console.log(i,'+',a,'=',i + a,)
+// }
+
+//8
+// let a = 3;
+// let b = 15;
+// for (let i=1;i<=b;i++) {
+//     console.log(i,'*',a,'=',i*a)
+// }
+
+//9
+// let a = 4;
+// let b = 12;
+// for (let i=1;i<=b;i++) {
+//     console.log(i,'+',a,'=',(i*i) + a )
+// }
+
+//10 ???
+// let n = 5;
+// let sum = 0;
+// for (let i=1;i<=n;i++) {
+//    console.log(sum+=1/i);
+// }
+
+//11
+
+// let n = 4;
+// let sum = 0;
+// for (let i=0;i<=n;i++) {
+//     console.log(sum+=Math.pow((n+i),2));
+// }
+
+// while va do while ///////
+
+///1 
+// let a = 12;
+// let b = 7;
+// let i = b;
+// let qoldiq;
+// while (i<=a) {
+//     qoldiq = a - i;
+//     i = i + b;
+// }
+// console.log(qoldiq)
+
+//2
+// let a = 8;
+// let b = 4;
+// let sum = 0;
+// let i = b;
+// while (i<=a) {
+//     sum+=1;
+//     i = i + b;
+// }
+// console.log(sum);
+
+//3
+// let n = 9;
+// let k = 5;
+// let qoldiq;
+// let butun = 0;
+// let i = k
+// while (i<=n) {
+//     qoldiq = n - i;
+//     butun+=1;
+//     i = i+k;
+// }
+// console.log(qoldiq,butun);
+//4
+
+// let n = 9;
+// let daraja = 1;
+// let i = 0;
+// while (i<=n) {
+//     if (n%3==0) {
+//        console.log('3 ning darjasi')
+//     } else {console.log('emas')}
+//     i = i + daraja
+// }
+
+////////////////////////////////////////Amaliy topshirqlar
+//1
+// let num = 8;
+// if (num%2 !==1) {
+//     console.log(num+1)
+// }else {console.log(num)}
+
+//2
+// let a = 23;
+// let b =  12;
+// if (a>b) {
+//     console.log(a,'sonni katta b dan');
+// } else {console.log(b,'sonni katta a dan');}
+
+//3
+//formula ax2+bx+c=0
+// let a = 6
+// let b = 8
+// let c = -5
+// let D = ((b*b)-4*a*c)
+// if (D<0) {
+//     console.log('tenglama yechmga ega emas');
+// } else if (D==0) {
+//     let x = (-b + Math.sqrt(D)/(2*a))
+//     console.log(x,'tenglama bitta ildizga ega')
+// } else {
+//     let x1 = (-b - Math.sqrt(D)/(2*a))
+//     let x2 = (-b + Math.sqrt(D)/(2*a))
+//     console.log(x1, x2 ,'tenglama 2 ta ildizga ega')
+// }
+
+//4
+// for (let i=0;i<=100;i++) {
+//     console.log(i)
+// }
+
+//5
+// for (let i=1;i<=100;i++) {
+//     if(i%2 !==0) {
+//         console.log(i+i)
+//     }
+// }
+
+// const arr = [14,26,11,24,55,101,20]
+
+//
+//7
+// let n1 = 'qizil';
+// let n2 = 'sariq';
+// let n3 = 'yashil';
+// switch('yashil') {
+//     case n1:
+//         console.log('xarakatni toxtating');
+//         break;
+//     case n2:
+//         console.log('kuting');
+//         break;
+//     case n3:
+//         console.log('xarakatni boshlang')   
+//         break;     
+//     default:
+//         console.log('nomalum');
+// }
+
