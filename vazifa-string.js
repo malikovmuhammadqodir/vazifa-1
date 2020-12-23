ab8d1baf6e1a10ecca75d6aacdaa50b516c11c95
malikovmuhammadqodir <75750838+malikovmuhammadqodir@users.noreply.github.com>
2020-12-23 19:59:47 +0500

    Add files via upload

---
+++ vazifa-string.js
@@ -0,0 +1,39 @@
+
+
+//1 
+// let word = 'this is javascript code';
+// console.log(word.length);
+// let obj = {word:word.length};
+// console.log(obj);
+
+//2
+// function wordToUppperCase() {
+//     let word = 'toshkent';
+//     let sub = word.substr(1,7);
+//     let result = 'T'.concat(sub);
+//     console.log(result);
+// }
+// wordToUppperCase();
+
+//3
+// function nameSurname() {
+//     let fullName = 'Malikov Muhammadqodir';
+//     console.log(fullName.toUpperCase());
+// }
+// nameSurname();
+
+//cutdown Timer
+// function calculate() {
+// let kerakliSana = new Date(2020, 11 , 26).getTime();
+// let hozr = new Date();
+// const oraliqKun = kerakliSana - hozr;
+
+//     let kun = Math.floor(oraliqKun / 1000/60/60/24);
+//     let soat = Math.floor((oraliqKun / 1000/60/60)%24);
+//     let daiqa = Math.floor((oraliqKun / 1000/60)%60);
+//     let sekund = Math.floor((oraliqKun /1000)%60);
+   
+// console.log(kun+' kun '+soat+' soat '+daiqa+' daqiqa '+sekund+' sekund ','qoldi');
+
+//   }
+// calculate();
